daf29d6617ef8e90e0c25567fab1c016f96d3abe
Nahom <phillsamsung43@gmail.com>
2026-09-03 14:30:39 +0300

    5th update

--- components/home/HeroImage.tsx
+++ components/home/HeroImage.tsx
@@ -12,7 +12,7 @@ export default function HeroImage() {
         width={550}
         height={550}
         priority
-        sizes="(max-width: 1023px) 100vw, 550px"
+        sizes="(max-width: 550px) 100vw, 550px"
         className="relative z-10 object-contain"
       />
     </div>
